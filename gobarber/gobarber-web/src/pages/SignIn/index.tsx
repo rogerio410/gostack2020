f6543011fad68837b8335c7add7a095d86ac1aa4
Rogerio Silva <rogerio410@gmail.com>
2020-11-14 17:05:35 -0300

    Create context for auth

--- gobarber/gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber/gobarber-web/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react'
+import React, { useCallback, useContext, useRef } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
@@ -6,33 +6,48 @@ import * as Yup from 'yup'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content, Background } from './styles'
 
+import { AuthContext } from '../../context/AuthContext'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 import getValidationErrors from '../../utils/getValidationErrors'
 
+interface SignInFormData {
+  email: string
+  password: string
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({})
+  const { signIn, user } = useContext(AuthContext)
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um email válido'),
-        password: Yup.string().min(6, 'Senha Obrigatória')
-      })
+  console.log(user)
 
-      await schema.validate(data, {
-        abortEarly: false
-      })
-    } catch (error) {
-      const errors = getValidationErrors(error)
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({})
 
-      formRef.current?.setErrors(errors)
-    }
-  }, [])
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um email válido'),
+          password: Yup.string().min(4, 'Senha Obrigatória')
+        })
+
+        await schema.validate(data, {
+          abortEarly: false
+        })
+        // validation OK
+        signIn({ email: data.email, password: data.password })
+      } catch (error) {
+        const errors = getValidationErrors(error)
+
+        formRef.current?.setErrors(errors)
+      }
+    },
+    [signIn]
+  )
 
   return (
     <Container>
